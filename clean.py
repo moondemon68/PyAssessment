b3ec67bb408d0fb7dbe6600dbeb6bae09eca4e14
moondemon68 <moondemon68@gmail.com>
2022-03-08 20:56:11 +0700

    feat: 🎸 add cleaner program

---
+++ clean.py
@@ -0,0 +1,15 @@
+
+import os, shutil
+folders = ['res/', 'logs/']
+for folder in folders:
+	for filename in os.listdir(folder):
+		if filename == '.gitkeep':
+			continue
+		file_path = os.path.join(folder, filename)
+		try:
+			if os.path.isfile(file_path) or os.path.islink(file_path):
+				os.unlink(file_path)
+			elif os.path.isdir(file_path):
+				shutil.rmtree(file_path)
+		except Exception as e:
+			print('Failed to delete %s. Reason: %s' % (file_path, e))
